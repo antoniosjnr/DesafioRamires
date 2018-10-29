7ae4b33a2c6514af4cfcb1fb9c3881d19a902495
Antonio da silva Junior <antonio.junior@depssc.local>
2018-10-29 10:08:58 -0300

    minificando projeto

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
+const HtmlPluginRemove = require('html-webpack-plugin-remove');
 
 module.exports = {
   entry: './index',
@@ -26,10 +26,28 @@ module.exports = {
   },
   plugins: [
     new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({template: './index.html'}),
+    new HtmlWebpackPlugin({
+		template: './index.html',
+		minify   : {
+			html5                          : true,
+			collapseWhitespace             : true,
+			minifyCSS                      : true,
+			minifyJS                       : true,
+			minifyURLs                     : false,
+			removeAttributeQuotes          : true,
+			removeComments                 : true,
+			removeEmptyAttributes          : true,
+			removeOptionalTags             : true,
+			removeRedundantAttributes      : true,
+			removeScriptTypeAttributes     : true,
+			removeStyleLinkTypeAttributese : true,
+			useShortDoctype                : true
+  }
+		}),
 	new ExtractTextPlugin('css/styles.css'),
 	new OptimizeCssAssetsPlugin({
       assetNameRegExp: /styles\.css/g
-    })
+    }),
+	new HtmlPluginRemove(/<link rel[^>]*href="[^>]*[^>]*"[^>]*>/ig)
   ]
 };
